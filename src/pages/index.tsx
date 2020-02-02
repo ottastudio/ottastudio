59a962d82a8792bd5c22fdbc7472b696a19e0466
Taufik Oktama <oktamataufik@gmail.com>
2020-02-02 12:29:20 +0700

    Add title, description, and better styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import { style } from "typestyle";
 
 const Index: NextPage<{}> = () => {
-  return <div>Soon</div>;
+  const h1Style = style({
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  });
+  return (
+    <div>
+      <Head>
+        <title>Otta &amp; Studio's</title>
+        <meta
+          name="description"
+          content="Eclectic design languages for modern era."
+        />
+      </Head>
+      <h1 className={h1Style}>Work in progress</h1>
+    </div>
+  );
 };
 
 export default Index;
