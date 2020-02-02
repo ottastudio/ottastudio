06f6ff8fd6bc1eade1e67cc44ca3173368ec9314
Taufik Oktama <oktamataufik@gmail.com>
2020-02-03 00:56:28 +0700

    Create misc.ts

---
+++ src/lib/misc.ts
@@ -0,0 +1,8 @@
+export const staticLinks = [
+  { name: "Index", to: "/" },
+  { name: "About", to: "/about" }
+  // { name: "Archive", to: "/archive" },
+  // { name: "News", to: "/news" },
+  // { name: "Exhibition", to: "/exhibition" },
+  // { name: "Privacy Policy", to: "/policy" }
+];
