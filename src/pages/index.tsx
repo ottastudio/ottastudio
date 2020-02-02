7e9039ebce3a304b1b5d258f5704c02dbb8761c7
Taufik Oktama <oktamataufik@gmail.com>
2020-02-02 12:47:19 +0700

    Add basic meta social media

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,30 @@ const Index: NextPage<{}> = () => {
     left: "50%",
     transform: "translate(-50%, -50%)"
   });
+
+  const canonical =
+    typeof window !== "undefined" && window && window.location.origin;
+
   return (
     <div>
       <Head>
         <title>Otta &amp; Studio's</title>
+        <link rel="canonical" href={canonical as string} />
         <meta
           name="description"
           content="Eclectic design languages for modern era."
+        />
+
+        <meta property="og:title" content="Otta & Studio's" />
+        <meta name="og:url" content={canonical as string} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="og:description"
+          content="Eclectic design languages for modern era."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dpfd7jmay/image/upload/v1567080499/samples/board_hrlzgu.jpg"
         />
       </Head>
       <h1 className={h1Style}>Work in progress</h1>
