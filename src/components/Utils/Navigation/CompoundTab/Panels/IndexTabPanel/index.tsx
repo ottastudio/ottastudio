027288045a7bc5a977d8e53839dcae445d081a9b
Taufik Oktama <oktamataufik@gmail.com>
2020-02-04 22:52:17 +0700

    Conditionally render static links

--- src/components/Utils/Navigation/CompoundTab/Panels/IndexTabPanel/index.tsx
+++ src/components/Utils/Navigation/CompoundTab/Panels/IndexTabPanel/index.tsx
@@ -1,7 +1,8 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { style } from "typestyle";
+import cookie from "js-cookie";
 
-import { staticLinks } from "../../../../../../lib/misc";
+import { staticLinks, userLinks, adminLinks } from "../../../../../../lib/misc";
 import { NavlinkRegular } from "../../../../NavLink";
 
 import TabPanel from "../../../Tab/TabPanel";
@@ -9,6 +10,7 @@ import TabPanel from "../../../Tab/TabPanel";
 export interface IndexTabPanelProps {}
 
 const IndexTabPanel: React.FC<IndexTabPanelProps> = () => {
+  const [token, setToken] = useState<string | undefined>();
   const divStyle = style({
     $debugName: "index-panel",
     minHeight: 120,
@@ -17,15 +19,25 @@ const IndexTabPanel: React.FC<IndexTabPanelProps> = () => {
     borderTop: "1px solid",
     marginTop: -1
   });
+
+  const linksToMap =
+    token === undefined
+      ? staticLinks.concat(userLinks)
+      : staticLinks.concat(adminLinks);
+
+  useEffect(() => {
+    const getCookie = cookie.get("token");
+    setToken(getCookie);
+  });
   return (
     <TabPanel whenActive="index">
       <div className={divStyle}>
-        {staticLinks.map(({ name, to }, i: number) => (
+        {linksToMap.map(({ name, to }, i: number) => (
           <Fragment key={i}>
             <NavlinkRegular href={to} activeClassName="link-active">
               <a className="link">{name}</a>
             </NavlinkRegular>
-            <span>{i >= staticLinks.length - 1 ? "." : ", "}</span>
+            <span>{i >= linksToMap.length - 1 ? "." : ", "}</span>
           </Fragment>
         ))}
       </div>
