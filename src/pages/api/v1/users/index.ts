d7483d3c71aaa4b949d94bf7b6937ae27a1f2fdf
Taufik Oktama <oktamataufik@gmail.com>
2020-02-05 13:26:22 +0700

    Add condditional quey

--- src/pages/api/v1/users/index.ts
+++ src/pages/api/v1/users/index.ts
@@ -2,12 +2,19 @@ import { withDB } from "../../../../lib/database";
 import { useModels } from "../../../../lib/database/useModels";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const users = async (_req: NextApiRequest, res: NextApiResponse) => {
+const users = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { query } = req;
   const { User } = await useModels();
 
+  const findBy = query.id
+    ? { _id: query.id }
+    : query.token
+    ? { token: query.token }
+    : {};
+
   try {
-    const users = await User.find({});
-    return res.status(200).json({ success: true, users });
+    const users = await User.find(findBy);
+    return res.status(200).json({ success: true, users: users });
   } catch (error) {
     return res.status(400).json({ success: false, error });
   }
