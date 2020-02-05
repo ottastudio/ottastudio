63661a44a3c9d9f9bc9ab1f64d3cb45f1e49ea9f
Taufik Oktama <oktamataufik@gmail.com>
2020-02-05 13:32:43 +0700

    Add cube component

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,17 +1,27 @@
 import { NextPage } from "next";
 import { useEffect } from "react";
+import { style } from "typestyle";
 import Head from "next/head";
 import cookie from "js-cookie";
 import Router from "next/router";
+import dynamic from "next/dynamic";
 
+import Typing from "../../components/Utils/Loader/Typing";
 import LoginForm from "../../components/Users/Administration/Login";
-import { style } from "typestyle";
+
+const Loader = () => <Typing />;
+
+const Cube = dynamic(() => import("../../components/Sandbox/Cube"), {
+  ssr: false,
+  loading: Loader
+});
 
 const Login: NextPage<{}> = () => {
   const divStyle = style({
     $debugName: "container-login",
-    height: "100vh",
-    position: "relative"
+    height: "50vh",
+    position: "relative",
+    zIndex: 10
   });
   useEffect(() => {
     const getCookie = cookie.get("token");
@@ -22,6 +32,7 @@ const Login: NextPage<{}> = () => {
       <Head>
         <title>Login</title>
       </Head>
+      <Cube color="deeppink" ambient={0.5} scale={2} />
       <LoginForm />
     </div>
   );
