18c56325259b7ca96a4f253f8f7d1692f910102d
Taufik Oktama <oktamataufik@gmail.com>
2020-02-05 13:32:58 +0700

    Improved with shared data

--- src/components/Users/Administration/Dashboard/dashboardLayout.tsx
+++ src/components/Users/Administration/Dashboard/dashboardLayout.tsx
@@ -1,45 +1,59 @@
+import { Fragment } from "react";
 import { style } from "typestyle";
 import { logout } from "../../../_HOC/auth";
 import { transition } from "../../../../lib/misc";
+import Typing from "../../../Utils/Loader/Typing";
 
-export interface DashboardLayoutProps {}
-
-const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
-  const divStyle = style({
-    minHeight: "100vh"
-  });
-  const headerStyle = style({
-    position: "sticky",
-    top: 0,
-    padding: 20
-  });
-  const buttonStyle = style({
-    $debugName: "logout-button",
-    height: 40,
-    background: "none",
-    color: "currentColor",
-    fontFamily: "inherit",
-    fontSize: "inherit",
-    cursor: "pointer",
-    border: "1px solid",
-    borderRadius: 50,
-    padding: "0px 15px",
-    transition: `background-color ${transition.main}`,
-    $nest: {
-      "&:hover": {
-        backgroundColor: "coral"
-      }
+export interface DashboardLayoutProps {
+  globalData?: any;
+}
+const headerStyle = style({
+  $debugName: "header-dashboard",
+  position: "sticky",
+  top: 0,
+  padding: 20,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  zIndex: 999
+});
+const buttonStyle = style({
+  $debugName: "logout-button",
+  height: 40,
+  background: "none",
+  color: "currentColor",
+  fontFamily: "inherit",
+  fontSize: "inherit",
+  cursor: "pointer",
+  border: "none",
+  padding: "0px 15px",
+  transition: `background-color ${transition.main}`,
+  $nest: {
+    "&:hover": {
+      textDecoration: "underline"
     }
-  });
+  }
+});
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({
+  children,
+  globalData
+}) => {
+  const {
+    user: { name }
+  } = globalData;
   return (
-    <div className={divStyle}>
-      <header className={headerStyle}>
+    <Fragment>
+      <div className={headerStyle}>
+        <span>
+          Hello, {name}
+          <Typing />
+        </span>
         <button className={buttonStyle} onClick={logout}>
           Logout
         </button>
-      </header>
+      </div>
       {children}
-    </div>
+    </Fragment>
   );
 };
 
--- src/pages/user/dashboard/index.tsx
+++ src/pages/user/dashboard/index.tsx
@@ -1,17 +1,47 @@
+import dynamic from "next/dynamic";
 import { NextPage } from "next";
 import { withAuthSync } from "../../../components/_HOC/auth";
 import Head from "next/head";
 import DashboardLayout from "../../../components/Users/Administration/Dashboard/dashboardLayout";
+import Typing from "../../../components/Utils/Loader/Typing";
 
-export interface DashboardProps {}
+const Loader = () => (
+  <div style={{ position: "fixed", top: 0, left: 0 }}>
+    <Typing />
+  </div>
+);
 
-const Dashboard: NextPage<DashboardProps> = () => {
+const Cube = dynamic(() => import("../../../components/Sandbox/Cube"), {
+  ssr: false,
+  loading: Loader
+});
+
+type User = {
+  _id?: string;
+  name?: string;
+  role?: number;
+  email?: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
+export interface DashboardProps {
+  token: string;
+  user: User;
+}
+
+const Dashboard: NextPage<DashboardProps> = props => {
+  const { user } = props;
   return (
-    <DashboardLayout>
+    <DashboardLayout globalData={props}>
       <Head>
         <title>Administrator Dashboard</title>
       </Head>
-      <h1>Dashboard</h1>
+      <Cube />
+      <div style={{ minHeight: "150vh", padding: 20 }}>
+        <h1>Dashboard</h1>
+        <div>{user.name}</div>
+        <div>{user.email}</div>
+      </div>
     </DashboardLayout>
   );
 };
