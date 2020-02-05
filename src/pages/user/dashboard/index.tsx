0aefbe42e6e32100a7ae8b2d74ca5f5d30fe48ad
Taufik Oktama <oktamataufik@gmail.com>
2020-02-05 20:49:29 +0700

    Add user list component

--- src/pages/user/dashboard/index.tsx
+++ src/pages/user/dashboard/index.tsx
@@ -1,9 +1,13 @@
 import dynamic from "next/dynamic";
 import { NextPage } from "next";
-import { withAuthSync } from "../../../components/_HOC/auth";
 import Head from "next/head";
+
+import useRequest from "../../../lib/hooks/useRequest";
+
+import { withAuthSync } from "../../../components/_HOC/auth";
 import DashboardLayout from "../../../components/Users/Administration/Dashboard/dashboardLayout";
 import Typing from "../../../components/Utils/Loader/Typing";
+import UserList from "../../../components/Users/Administration/UserList";
 
 const Loader = () => (
   <div style={{ position: "fixed", top: 0, left: 0 }}>
@@ -27,21 +31,36 @@ type User = {
 export interface DashboardProps {
   token: string;
   user: User;
+  users: any;
+  subscribers: any;
 }
 
 const Dashboard: NextPage<DashboardProps> = props => {
-  const { user } = props;
+  const { user, users, subscribers } = props;
+  const { data: usersRegistered, revalidate: revalidateUsers } = useRequest(
+    { url: "/api/v1/users" },
+    { initialData: users }
+  );
+  const { data: subscriber, revalidate: revalidateSubscriber } = useRequest(
+    { url: "/api/v1/users/subscribe" },
+    { initialData: subscribers }
+  );
   return (
     <DashboardLayout globalData={props}>
       <Head>
         <title>{user.name}</title>
       </Head>
       <Cube />
-      <div style={{ minHeight: "150vh", padding: 20 }}>
-        <h1>Dashboard</h1>
+      <div style={{ padding: 20 }}>
         <div>{user.name}</div>
         <div>{user.email}</div>
       </div>
+      <UserList
+        users={usersRegistered.users}
+        revalidateUsers={revalidateUsers}
+        subscribers={subscriber.response}
+        revalidateSubscriber={revalidateSubscriber}
+      />
     </DashboardLayout>
   );
 };
