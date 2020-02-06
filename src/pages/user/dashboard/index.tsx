eae035d8ac6256550a33bff481537775a997c6d2
Taufik Oktama <oktamataufik@gmail.com>
2020-02-06 23:58:18 +0700

    Removed webGL component

--- src/pages/user/dashboard/index.tsx
+++ src/pages/user/dashboard/index.tsx
@@ -1,27 +1,14 @@
-// import dynamic from "next/dynamic";
 import { NextPage } from "next";
 import Head from "next/head";
 
 import useRequest from "../../../lib/hooks/useRequest";
 
 import { withAuthSync } from "../../../components/_HOC/auth";
-import DashboardLayout from "../../../components/Users/Administration/Dashboard/dashboardLayout";
-// import Typing from "../../../components/Utils/Loader/Typing";
+import { DashboardLayout } from "../../../components/Users/Administration/Dashboard/dashboardLayout";
 import UserList from "../../../components/Users/Administration/UserList";
 
-// const Loader = () => (
-//   <div style={{ position: "fixed", top: 0, left: 0 }}>
-//     <Typing />
-//   </div>
-// );
-
-// const Cube = dynamic(() => import("../../../components/Sandbox/Cube"), {
-//   ssr: false,
-//   loading: Loader
-// });
-
 type User = {
-  _id?: string;
+  _id: string;
   name?: string;
   role?: number;
   email?: string;
@@ -39,11 +26,11 @@ const Dashboard: NextPage<DashboardProps> = props => {
   const { user, users, subscribers } = props;
   const { data: usersRegistered, revalidate: revalidateUsers } = useRequest(
     { url: "/api/v1/users" },
-    { initialData: users }
+    { initialData: users, revalidateOnFocus: true }
   );
   const { data: subscriber, revalidate: revalidateSubscriber } = useRequest(
     { url: "/api/v1/users/subscribe" },
-    { initialData: subscribers }
+    { initialData: subscribers, revalidateOnFocus: true }
   );
 
   return (
@@ -51,7 +38,6 @@ const Dashboard: NextPage<DashboardProps> = props => {
       <Head>
         <title>{user.name}</title>
       </Head>
-      {/* <Cube /> */}
       <div style={{ padding: 20 }}>
         <div>{user.name}</div>
         <div>{user.email}</div>
