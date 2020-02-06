f2e4dd5c8216ae4317041df953a107583393e2d6
Taufik Oktama <oktamataufik@gmail.com>
2020-02-06 23:55:49 +0700

    Conditional render component

--- src/components/Utils/Loader/OnlineIndicator/index.tsx
+++ src/components/Utils/Loader/OnlineIndicator/index.tsx
@@ -1,19 +1,24 @@
 import Typing from "../Typing";
 import { style } from "typestyle";
+import { UIContext } from "../../../../lib/store/UIContext";
+import { useContext } from "react";
 
 const OnlineIndicator: React.FC<{}> = () => {
+  const { loading } = useContext(UIContext);
   const divStyle = style({
     $debugName: "online-indicator",
     position: "fixed",
     right: 20,
-    bottom: 20,
-    zIndex: 1000
+    top: 20,
+    zIndex: 1000,
+    display: loading ? "block" : "none"
   });
-  return (
+
+  return loading ? (
     <div className={divStyle}>
       <Typing />
     </div>
-  );
+  ) : null;
 };
 
 export default OnlineIndicator;
