d23b002ca7a1009aed5725c7a516f484118764c1
Taufik Oktama <oktamataufik@gmail.com>
2020-02-07 22:18:37 +0700

    Consumee AppContext and improved global classname

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { NextPage, NextPageContext, NextComponentType } from "next";
 import { NextRouter } from "next/router";
 import { transition } from "../lib/misc";
-import { UIProvider } from "../lib/store/UIContext";
+import { Ottastudio } from "../lib/store";
 
 import Navigation from "../components/Utils/Navigation";
 import NProgress from "../components/Utils/Loader/NProgress";
@@ -17,7 +17,7 @@ interface AppProps {
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps, router }) => {
   return (
-    <UIProvider>
+    <Ottastudio>
       <OnlineIndicator />
       <Navigation />
       <Component {...pageProps} key={router.asPath} />
@@ -119,6 +119,9 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps, router }) => {
           text-decoration: none;
           color: inherit;
         }
+        button {
+          cursor: pointer;
+        }
         table {
           border-collapse: collapse;
           width: 100%;
@@ -152,13 +155,26 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps, router }) => {
           text-decoration: underline;
         }
 
-        .theme-light {
+        .theme--light,
+        .theme--white {
           background-color: #f5f5f5;
           color: #000000;
         }
-        .theme-dark {
+        .theme--dark {
           background-color: #808080;
           color: #d4d4d4;
+        }
+        .theme--black {
+          background-color: #151515;
+          color: #d4d4d4;
+        }
+        .theme--red {
+          background-color: #ff0000;
+          color: #000000;
+        }
+        .theme--blue {
+          background-color: #0000ff;
+          color: #f5f5f5;
         }
 
         @media screen and (max-width: 767px) {
@@ -169,28 +185,28 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps, router }) => {
 
         @media (prefers-color-scheme: light) {
           body,
-          .theme-light {
+          .theme--light {
             background-color: #f5f5f5;
             color: #000000;
           }
-          .theme-dark {
+          .theme--dark {
             background-color: #808080;
             color: #d4d4d4;
           }
         }
         @media (prefers-color-scheme: dark) {
           body,
-          .theme-light {
+          .theme--light {
             background-color: #d4d4d4;
             color: #000000;
           }
-          .theme-dark {
+          .theme--dark {
             background-color: #151515;
             color: #d4d4d4;
           }
         }
       `}</style>
-    </UIProvider>
+    </Ottastudio>
   );
 };
 
