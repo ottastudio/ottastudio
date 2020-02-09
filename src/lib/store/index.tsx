30c9323c611db129ffc951ca79d6d10f82db8acd
Taufik Oktama <oktamataufik@gmail.com>
2020-02-09 13:49:21 +0700

    Add new context

--- src/lib/store/index.tsx
+++ src/lib/store/index.tsx
@@ -1,12 +1,18 @@
 import { createContext } from "react";
+import { AuthProvider } from "./AuthContext";
 import { UIProvider } from "./UIContext";
+import { NotificationProvider } from "./NotificationContext";
 
 const AppContext = createContext({});
 
 export const Ottastudio: React.FC<{}> = ({ children }) => {
   return (
     <AppContext.Provider value={{ lang: "en" }}>
-      <UIProvider>{children}</UIProvider>
+      <AuthProvider>
+        <UIProvider>
+          <NotificationProvider>{children}</NotificationProvider>
+        </UIProvider>
+      </AuthProvider>
     </AppContext.Provider>
   );
 };
