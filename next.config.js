07dfb12dadfbe31b81f079e18baa00ca13449d87
Taufik Oktama <oktamataufik@gmail.com>
2020-02-09 13:57:47 +0700

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withPlugins = require("next-compose-plugins");
 const dotenvLoad = require("dotenv-load");
 const fonts = require("next-fonts");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+});
 
 dotenvLoad();
 
 const nextConfig = {};
-module.exports = withPlugins([[fonts]], nextConfig);
+module.exports = withPlugins([[fonts], [withBundleAnalyzer]], nextConfig);
