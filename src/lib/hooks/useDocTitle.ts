8981add6c9f5abd07839c17a21f5eff33d68bffe
ottta <oktamataufik@gmail.com>
2020-05-25 17:12:10 +0700

    Add title type string

--- src/lib/hooks/useDocTitle.ts
+++ src/lib/hooks/useDocTitle.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export const useDocTitle = () => {
-  const [title, setTitle] = useState();
+  const [title, setTitle] = useState<string>();
   useEffect(() => {
     const getTitle = setTimeout(() => {
       setTitle(document.title);
